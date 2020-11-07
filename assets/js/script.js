6e7ed7ce914767341cc425850e0e69375c895382
Joey Sisk <jjmsisk@gmail.com>
2020-11-07 16:49:22 -0600

    updates notes and file structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,67 @@
-// thse constants get the location of the divs to show or hide in the html 
+// location of the divs to show or hide in the html 
 const startScreen = document.getElementById("startDiv");
 const questionScreen = document.getElementById("quizDiv");
 const leaderboardScreen = document.getElementById("leaderboardDiv");
 
-// this locates the start button from the html
+// start button from the html
 const startButton = document.getElementById("startBtn");
 
-// this locates areas where we will load questions and answers onto the screen in the html 
+// areas where we will load questions and answers onto the screen in the html 
 const questionPlace = document.getElementById("questionLocation");
 const answerOption1 = document.getElementById("answerA");
 const answerOption2 = document.getElementById("answerB");
 const answerOption3 = document.getElementById("answerC");
 const answerOption4 = document.getElementById("answerD");
 
-// this locates where the user will enter their name and where it will be loaded
+// input and output locations
 const usernameForm = document.getElementById("usersNameContainer");
 const usernameInput = document.getElementById("usersNameInput");
 const scoresPlace = document.getElementById("scoresList");
 
-// this locates where the timer will be displayed for the user
+// where the timer will be displayed for the user
 const clock = document.getElementById("timer");
 
-// this defines the total number of questions
+// total number of questions
 const totalQuestionsNumber = questions.length;
 
-// this sets the countdown timer based on how long of a quiz the user will be completeing
+// set the countdown timer based on how long of a quiz the user will be completeing
 let countdownTimer = ((questions.length * 20) + 1);
 
-// this is how much the timer will be reduced when a wrong answer is picked
+// how much the timer will be reduced when a wrong answer is picked
 let penalty = (questions.length * 3);
 
-// this is where the final score will be loaded
+// where the final score will be loaded
 let finalScore;
 
-// this is how the program keeps track of what question the user is on
+// how the program keeps track of what question the user is on
 let currentQuestionNumber = 0;
 
-// this holds all the usernames that should be loaded to the leaderboard, **replace with local storage**
+// all the usernames that should be loaded before local storage
 let usersNames = [];
 
 // this holds the winning score
 let winScore;
 
+// tracks whether the clock is stopped or not
 let stopClock;
 
-let playerScore = 0;
-
+// temporary storage for user input
 let currentStorage = [];
 
 function startQuiz() {
   // resets the score for new runs
   winScore = 0;
 
-  playerScore = 0;
-
-  // this will hide the initial starting div
+  // hide the initial starting div
   startScreen.setAttribute("class", "hide");
 
-  // this will uhide the questions div and keep the positiong
+  // uhide the questions div and keep the positiong
   questionScreen.classList.remove("hide");
   
-  // this will start the function to populate the questions and answers
+  //start the function to populate the questions and answers
   askQuestions();
   
-  // this will begin the function that counts down and moves the user to the leaderboard screen
+  // begin the function that counts down and moves the user to the leaderboard screen
   startTimer();
 }
 
@@ -84,22 +82,22 @@ function startTimer() {
   }, 1000);
 }
 
-// this function is what actually loads questions onto the screen
+// this actually loads questions onto the screen
 function askQuestions() {
-  // this is how I know which question the user is currently on
+  // how we know which question the user is currently on
   let currentQuestion = questions[currentQuestionNumber];
 
-  // the title is pulled from the questions variable
+  // title is pulled from the questions variable
   questionPlace.textContent = currentQuestion.questionTitle;
   
-  // the answer buttons are loaded up individually with answers from the questions variable
+  // answer buttons are loaded up individually with answers from the questions variable
   answerOption1.textContent = currentQuestion.answerOptions[0];
   answerOption2.textContent = currentQuestion.answerOptions[1];
   answerOption3.textContent = currentQuestion.answerOptions[2];
   answerOption4.textContent = currentQuestion.answerOptions[3];
 }
 
-// this function checks whether the answer picked was correct or not
+// check whether the answer picked was correct or not
 function checkAnswer() {
   // the text in between the button tags will be loaded into this variable
   let buttonContents = this.innerHTML;
@@ -133,14 +131,14 @@ function checkAnswer() {
   }
 }
 
-// this sets the color of the timer back to #black after 2 seconds
+// set the color of the timer back to #black after 2 seconds
 function turnRed() {
   setTimeout(function(){
     clock.classList.remove("redAlert");
   }, 1200);
 }
 
-// this hides the questions div and shows the leaderboard div
+// hide the questions div and shows the leaderboard div
 function leaderboardPageLoad() {
 
   questionScreen.setAttribute("class", "hide")
@@ -148,55 +146,63 @@ function leaderboardPageLoad() {
   leaderboardScreen.classList.remove("hide");  
 }
 
-// this fills out the leaderboard with the users name and current score and shows all previous scores
+// fill out the leaderboard with the users name and current score and shows all previous scores
 function leaderboardLoad() {
   while (scoresPlace.firstChild) {
         scoresPlace.removeChild(scoresPlace.firstChild);
   }
   
+  // convert the local memory to an object
   let storedUsers = JSON.parse(localStorage.getItem("scores"));
 
+  // run through the objects in storage and print to the storedUsers ul
   for (var i = 0; i < storedUsers.length; i ++) {
     let li = document.createElement("li");
-
     
+    // builds the list items
     li.textContent = (storedUsers[i].usersName + ": " + storedUsers[i].winningScore);
     scoresPlace.appendChild(li);
   }
 }
 
-// this listens for if the user has filled in the top score form
+// listen for if the user has filled in the top score form
 usernameForm.addEventListener("submit", function(event) {
-  // this keeps the form from defaulting to its base html functionality
+  // keep the form from defaulting to its base html functionality
   event.preventDefault();
 
-  // this is where we temporarily keep the users input
+  // we temporarily keep the users input
   let usernameText = usernameInput.value.trim();
 
-  // this stops the user from being able to enter an empty string into the input
+  // stop the user from being able to enter an empty string into the input
   if (usernameText === "") {
     return;
   }
 
+  // reset the timer display
   clock.innerHTML = "0";
 
-  // this adds the input into the previous variable
+  // add the input into the previous variable
   usersNames = {
     usersName: usernameText,
   	winningScore: winScore
   };
   
+  // either create a local storage place or load the one thats already there
   currentStorage = JSON.parse(localStorage.getItem("scores")) || [];
 
+  // add the new user inputs to the list
   currentStorage.push(usersNames);
 
+  // replace the storage with the old list plus the new input
   localStorage.setItem("scores", JSON.stringify(currentStorage));
   
+  // clear out usernameInput
   usernameInput.nodeValue = "";
 
   // this calls the function to add the name to the list
   leaderboardLoad();
 
+  // go back to the start screen
   startScreenLoad();
 });
 
